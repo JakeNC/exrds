80ec51f5df521f4c4274f53993399282b8bf1471
Jake <jake@office-deb.officedeb.com>
2018-02-07 20:34:08 -0500

    dynamic screen width

--- wp-includes/js/exrds-main.js
+++ wp-includes/js/exrds-main.js
@@ -8,45 +8,46 @@ jQuery(document).ready(function(){
         }, 800, 'linear');
     });
 
+    console.log(screen.width);
 
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         alert('you\'re on mobile');
     } else {
-        alert('you\'re on a computer');
+        // make some waves.
+        var ocean = document.getElementById("ocean"),
+            waveWidth = 10,
+            waveCount = Math.floor(screen.width/waveWidth),
+            docFrag = document.createDocumentFragment();
+        for(var i = 0; i < waveCount; i++){
+            var wave = document.createElement("div");
+            wave.className += " wave";
+            docFrag.appendChild(wave);
+            wave.style.left = i * waveWidth + "px";
+            wave.style.webkitAnimationDelay = (i/100) + "s";
+
+            var wave_middle = document.createElement("div");
+            wave_middle.className += " wave_middle";
+            docFrag.appendChild(wave_middle);
+            wave_middle.style.left = i * waveWidth + "px";
+            wave_middle.style.webkitAnimationDelay = (i/91) + "s";
+
+            var wave_bottom = document.createElement("div");
+            wave_bottom.className += " wave_bottom";
+            docFrag.appendChild(wave_bottom);
+            wave_bottom.style.left = i * waveWidth + "px";
+            wave_bottom.style.webkitAnimationDelay = (i/97) + "s";
+
+            var wave_light = document.createElement("div");
+            wave_light.className += " wave_light";
+            docFrag.appendChild(wave_light);
+            wave_light.style.left = i * waveWidth + "px";
+            wave_light.style.webkitAnimationDelay = 0 + "s";
+
+        }
+        ocean.appendChild(docFrag);
     }
 
-// make some waves.
-    var ocean = document.getElementById("ocean"),
-        waveWidth = 10,
-        waveCount = Math.floor(2500/waveWidth),
-        docFrag = document.createDocumentFragment();
-    for(var i = 0; i < waveCount; i++){
-        var wave = document.createElement("div");
-        wave.className += " wave";
-        docFrag.appendChild(wave);
-        wave.style.left = i * waveWidth + "px";
-        wave.style.webkitAnimationDelay = (i/100) + "s";
 
-        var wave_middle = document.createElement("div");
-        wave_middle.className += " wave_middle";
-        docFrag.appendChild(wave_middle);
-        wave_middle.style.left = i * waveWidth + "px";
-        wave_middle.style.webkitAnimationDelay = (i/91) + "s";
-
-        var wave_bottom = document.createElement("div");
-        wave_bottom.className += " wave_bottom";
-        docFrag.appendChild(wave_bottom);
-        wave_bottom.style.left = i * waveWidth + "px";
-        wave_bottom.style.webkitAnimationDelay = (i/97) + "s";
-
-        var wave_light = document.createElement("div");
-        wave_light.className += " wave_light";
-        docFrag.appendChild(wave_light);
-        wave_light.style.left = i * waveWidth + "px";
-        wave_light.style.webkitAnimationDelay = 0 + "s";
-
-    }
-    ocean.appendChild(docFrag);
 
 
 
